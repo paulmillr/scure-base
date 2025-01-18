a122dbad74c8f641e914465da6f85dc8e346d9ac
Paul Miller <paul@paulmillr.com>
2025-01-18 01:51:33 +0000

    Bump jsbt helpers 0.3.0. Add build:release script.

---
+++ test/build/input.js
@@ -0,0 +1,11 @@
+export { base16, base32, base64, base58 } from '@scure/base';
+export {
+  base58xmr,
+  base58xrp,
+  base32hex,
+  base32crockford,
+  base64nopad,
+  base64url,
+  base64urlnopad,
+} from '@scure/base';
+export { createBase58check, bech32, bech32m, hex } from '@scure/base';
